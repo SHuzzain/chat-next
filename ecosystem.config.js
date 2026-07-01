551accce6579039c43ca48f276d09057db9b6aef
shahin s. hussain <shahin.s@owcdomain.com>
2026-07-01 16:33:35 +0530

    chore: rename application in PM2 config from 'chatbot-next' to 'lms-chatbot-next' and standardize string quotes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'chatbot-next',
-      script: './node_modules/.bin/next',
-      args: 'start -p 3606',
+      name: "lms-chatbot-next",
+      script: "./node_modules/.bin/next",
+      args: "start -p 3606",
       instances: 1,
-      exec_mode: 'fork',
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: "1G",
       time: true,
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
         PORT: 3606,
       },
     },
